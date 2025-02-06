7704caa99eae4f9fece6a3f8dd13d091d65f6c8b
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-02-05 23:10:50 -0400

    more styles

--- components/download_dialog.py
+++ components/download_dialog.py
@@ -7,11 +7,12 @@ def download_dialog(filepath: str, key_path: str, name: str, description: str):
         st.write("Are you sure you want to download this file?")
         decrypted_data = decrypt_file(filepath, key_path)
         st.download_button(
-            "",
+            "Download file",
             data=decrypted_data,
             file_name="out.txt",
             icon=":material/download_2:",
-            key=f"{name}-{description}-download-final"
+            key=f"{name}-{description}-download-final",
+            use_container_width=True
         )
     except Exception as error:
         st.error(f"Error al procesar la descarga: {error}")
