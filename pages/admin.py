bfb9635ccf07fd82c48de8d6cecdc938316fccc7
Atla004 <andrese.g.v.13579@gmail.com>
2025-02-05 21:48:02 -0400

    feat: enable admin cards display with updated logic

--- pages/admin.py
+++ pages/admin.py
@@ -4,11 +4,11 @@ from services.supabase_client import supabase
 
 st.header("Admin's Licitations")
 
-# cards = {
-#     "lict1": "approved",
-#     "lict2": "rejected",
-#     "lict3": "pending"
-# }
+cards = {
+    "lict1": "approved",
+    "lict2": "rejected",
+    "lict3": "pending"
+}
 
-# for title, state in cards.items():
-#     admin_card(title, "Asdasd"*32)
+for title, state in cards.items():
+    admin_card(title, "Asdasd"*32, "files/licitation", "files/key")
