fdebe1d1cf85166db91faf9c48b2fc3228a9ae1f
Rafael Mata <83139503+Rafa-Mt@users.noreply.github.com>
2025-02-05 23:08:14 -0400

    feat: maybe this works(?

--- components/admin_card.py
+++ components/admin_card.py
@@ -1,8 +1,7 @@
 import streamlit as st
 from components.approve_dialog import approve_dialog
+from components.download_dialog import download_dialog
 from services.encrypter import decrypt_file
-import os 
-
 
 def admin_card(name: str, description: str, filepath: str, key_path: str):
     container = st.container()
@@ -12,18 +11,8 @@ def admin_card(name: str, description: str, filepath: str, key_path: str):
     left.subheader(description)
     center.button("Change State", on_click=approve_dialog, key=f"{name}-{description}-set")
 
-    if right.button("Decrypt",on_click=  key=f"{name}-{description}-download"):
-        try:
-            print("aaaaaaaaaaaaaa")
-            decrypted_data = decrypt_file(filepath, key_path)
-            right.download_button(
-                "",
-                data=decrypted_data,
-                file_name="out.txt",
-                icon=":material/download_2:",
-                key=f"{name}-{description}-download-final"
-            )
-        except Exception as error:
-            st.error(f"Error al procesar la descarga: {error}")
+    if right.button("Decrypt", key=f"{name}-{description}-download"):
+        download_dialog(filepath, key_path, name, description)
+       
 
     
---
+++ components/download_dialog.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from services.encrypter import decrypt_file
+
+@st.dialog('download')
+def download_dialog(filepath: str, key_path: str, name: str, description: str):
+    try:
+        st.write("Are you sure you want to download this file?")
+        decrypted_data = decrypt_file(filepath, key_path)
+        st.download_button(
+            "",
+            data=decrypted_data,
+            file_name="out.txt",
+            icon=":material/download_2:",
+            key=f"{name}-{description}-download-final"
+        )
+    except Exception as error:
+        st.error(f"Error al procesar la descarga: {error}")
