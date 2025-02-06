c08a905bdbfff95b7cfb29cd67895452cf00db56
Atla004 <andrese.g.v.13579@gmail.com>
2025-02-05 23:03:13 -0400

    depravate

---
+++ services/encrypter.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+
+def decrypt_file(filepath: str, key_path: str) -> bytes:
+    print(f"decrypting {filepath}  {key_path}")
+    os.system(f".\services\encrypter.exe -d {filepath} private.pem {key_path}")
+
+
+
+    out_file = "out.txt"
+
+    if not os.path.exists(out_file):
+        raise Exception("El archivo out.txt no se encontró después de la ejecución.")
+    
+    with open(out_file, "rb") as f:
+        file_data = f.read()
+    
+    os.remove(out_file)
+    
+    return file_data
